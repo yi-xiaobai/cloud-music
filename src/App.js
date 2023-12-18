3507e4c95ba22475589a604c3aa955350a1dba56
yi.luo <yi.luo@19pay.com>
2023-12-18 21:37:14 +0800

    feat:init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import { GlobalStyle } from "./style";
+// 引入路由配置文件
+import routes from "./routes";
+// 读取路由配置转为标签
+import {} from 'react-router-config'
 
 function App() {
   return (
---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import Home from "../Application/Home";
+import Recommend from "../Application/Recommend";
+import Singers from "../Application/Singers";
+import Rank from "../Application/Rank";
+
+export default [
+  {
+    path: "/",
+    components: Home,
+    routes: [
+      {
+        path: "/",
+        exact: true,
+        render: () => {
+          <Redirect to={"/recommend"} />;
+        },
+      },
+      {
+        path: "/recommend",
+        component: Recommend,
+      },
+      {
+        path: "/singers",
+        component: Singers,
+      },
+      {
+        path: "/rank",
+        component: Rank,
+      },
+    ],
+  },
+];
